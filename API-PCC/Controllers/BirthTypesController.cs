633c52b0f0e198de6b0d06832574802cde251c44
Leari John Barasona <learijohnb@gmail.com>
2024-05-01 10:35:12 +0800

    [BirthType] Change view endpoint to searchParam

--- API-PCC/Controllers/BirthTypesController.cs
+++ API-PCC/Controllers/BirthTypesController.cs
@@ -2,10 +2,19 @@
 using API_PCC.ApplicationModels;
 using API_PCC.ApplicationModels.Common;
 using API_PCC.Data;
+using API_PCC.Manager;
 using API_PCC.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
+using Org.BouncyCastle.Utilities;
+using System.Data;
+using System.Drawing.Printing;
+using System.Data;
+using System.Data.SqlClient;
+using API_PCC.Utils;
 namespace API_PCC.Controllers
 {
     [Authorize("ApiKey")]
@@ -14,15 +23,7 @@ namespace API_PCC.Controllers
     public class BirthTypesController : ControllerBase
     {
         private readonly PCC_DEVContext _context;
-
-        public class BirthTypesSearchFilter
-        {
-            public string? BirthTypeCode { get; set; }
-            public string? BirthTypeDesc { get; set; }
-            public int page { get; set; }
-            public int pageSize { get; set; }
-        }
-
+        DbManager db = new DbManager();
         public BirthTypesController(PCC_DEVContext context)
         {
             _context = context;
@@ -30,61 +31,65 @@ namespace API_PCC.Controllers
 
         // POST: BirthTypes/list
         [HttpPost]
-        public async Task<ActionResult<IEnumerable<ABirthType>>> list(BirthTypesSearchFilter searchFilter)
+        public async Task<ActionResult<IEnumerable<ABirthType>>> list(BirthTypesSearchFilterModel searchFilter)
         {
-          if (_context.ABirthTypes == null)
-          {
-                return Problem("Entity set 'PCC_DEVContext.BirthTypes' is null!");
+            try
+            {
+                DataTable queryResult = db.SelectDb_WithParamAndSorting(QueryBuilder.buildBirthTypeSearchQueryByBirthTypeCodeOrBirthTypeDesc(), null, populateSqlParameters(searchFilter));
+                var result = buildBirthTypesPagedModel(searchFilter, queryResult);
+                return Ok(result);
             }
+            catch (Exception ex)
+            {
+                return Problem(ex.GetBaseException().ToString());
+            }
+        }
+
+        private List<BirthTypesPagedModel> buildBirthTypesPagedModel(BirthTypesSearchFilterModel searchFilter, DataTable dt)
+        {
             int pagesize = searchFilter.pageSize == 0 ? 10 : searchFilter.pageSize;
             int page = searchFilter.page == 0 ? 1 : searchFilter.page;
             var items = (dynamic)null;
             int totalItems = 0;
             int totalPages = 0;
 
+            totalItems = dt.Rows.Count;
+            totalPages = (int)Math.Ceiling((double)totalItems / pagesize);
+            items = dt.AsEnumerable().Skip((page - 1) * pagesize).Take(pagesize).ToList();
 
-            var birthTypesList = _context.ABirthTypes.AsNoTracking();
-            birthTypesList = birthTypesList.Where(birthType => !birthType.DeleteFlag);
-            try
+            var result = new List<BirthTypesPagedModel>();
+            var item = new BirthTypesPagedModel();
+
+            int pages = searchFilter.page == 0 ? 1 : searchFilter.page;
+            item.CurrentPage = searchFilter.page == 0 ? "1" : searchFilter.page.ToString();
+            int page_prev = pages - 1;
+
+            double t_records = Math.Ceiling(Convert.ToDouble(totalItems) / Convert.ToDouble(pagesize));
+            int page_next = searchFilter.page >= t_records ? 0 : pages + 1;
+            item.NextPage = items.Count % pagesize >= 0 ? page_next.ToString() : "0";
+            item.PrevPage = pages == 1 ? "0" : page_prev.ToString();
+            item.TotalPage = t_records.ToString();
+            item.PageSize = pagesize.ToString();
+            item.TotalRecord = totalItems.ToString();
+            item.items = items;
+            result.Add(item);
+
+            return result;
+        }
+
+        private SqlParameter[] populateSqlParameters(BirthTypesSearchFilterModel searchFilterModel)
+        {
+
+            var sqlParameters = new List<SqlParameter>();
+
+            sqlParameters.Add(new SqlParameter
             {
-                if (searchFilter.BirthTypeCode != null && searchFilter.BirthTypeCode != "")
-                {
-                    birthTypesList = birthTypesList.Where(birthType => birthType.BirthTypeCode.Contains(searchFilter.BirthTypeCode));
-                }
+                ParameterName = "SearchParam",
+                Value = searchFilterModel.searchParam ?? Convert.DBNull,
+                SqlDbType = System.Data.SqlDbType.VarChar,
+            });
 
-                if (searchFilter.BirthTypeDesc != null && searchFilter.BirthTypeDesc != "")
-                {
-                    birthTypesList = birthTypesList.Where(birthType => birthType.BirthTypeDesc.Contains(searchFilter.BirthTypeDesc));
-                }
-
-                totalItems = birthTypesList.ToList().Count();
-                totalPages = (int)Math.Ceiling((double)totalItems / pagesize);
-                items = birthTypesList.Skip((page - 1) * pagesize).Take(pagesize).ToList();
-
-                var result = new List<BirthTypesPagedModel>();
-                var item = new BirthTypesPagedModel();
-
-                int pages = searchFilter.page == 0 ? 1 : searchFilter.page;
-                item.CurrentPage = searchFilter.page == 0 ? "1" : searchFilter.page.ToString();
-                int page_prev = pages - 1;
-
-                double t_records = Math.Ceiling(Convert.ToDouble(totalItems) / Convert.ToDouble(pagesize));
-                int page_next = searchFilter.page >= t_records ? 0 : pages + 1;
-                item.NextPage = items.Count % pagesize >= 0 ? page_next.ToString() : "0";
-                item.PrevPage = pages == 1 ? "0" : page_prev.ToString();
-                item.TotalPage = t_records.ToString();
-                item.PageSize = pagesize.ToString();
-                item.TotalRecord = totalItems.ToString();
-                item.items = items;
-                result.Add(item);
-                return Ok(result);
-            }
-
-            catch (Exception ex)
-            {
-                
-                return Problem(ex.GetBaseException().ToString());
-            }
+            return sqlParameters.ToArray();
         }
 
         // GET: BirthTypes/search/5
