2930731f2178e15abb75f0a270b64c8e9fcac8e0
Leari John Barasona <learijohnb@gmail.com>
2024-06-05 20:44:08 +0800

    fix authorization

--- API-PCC/Controllers/PedigreeController.cs
+++ API-PCC/Controllers/PedigreeController.cs
@@ -15,7 +15,7 @@ using System.Linq.Dynamic.Core;
 
 namespace API_PCC.Controllers
 {
-    //[Authorize("ApiKey")]
+    [Authorize("ApiKey")]
     [Route("[controller]/[action]")]
     [ApiController]
     public class PedigreeController : ControllerBase
